cd021ef00d4b919feb6c5b56dd72109c82bc1645
Otabek Askarov <65924250+Otabek-coder@users.noreply.github.com>
2021-04-07 08:27:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 const share_btn = document.querySelector(".share");
-const underArrow = document.querySelector(".under-arrow");
-const img_share = document.querySelector(".img-share");
 
 share_btn.addEventListener("click", () => {
-  share_btn.style.background = "hsl(217, 19%, 35%)";
 });
 
 function myFunction() {
   const cns_box = document.querySelector(".cns-box");
-  const proPic = document.querySelector(".profile-pic");
-  const details = document.querySelector(".details");
+  
   if (cns_box.style.display === "none") {
     cns_box.style.display = "flex";
 
